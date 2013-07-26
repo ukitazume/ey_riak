98dffed3f5caf2b39765740dc08446c30e44a21b
Yu Kitazume <u.kitazume@gmail.com>
2013-07-26 15:45:50 +0900

    add riak

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+require('js-yaml');
+
 var express = require('express');
 var app = express();
+var nodeEnv = process.env.NODE_ENV || 'development'
+var port = process.env.PORT || '3000'
 
-app.get('/', function(req, res){
-  var body = 'Hello World';
+try {
+  var doc = require('./config/ripple.yml')[nodeEnv]
+  var config = {host: doc.host, port: doc.http_port}
+} catch(e) {
+  console.log('nothing riak config at ./config/ripple.yml');
+  exit();
+}
+
+var riakDB = require('riak-js').getClient(config);
+
+
+app.get('/:key', function(req, res){
+  riakDB.get('params', req.params.key, function(e, f, m){
+    body = JSON.stringify(f);
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', body.length);
+    res.end(body);
+  });
+})
+
+app.get('/:key/:value', function(req, res){
+  riakDB.save('params', req.params.key, req.params.value);
+  body = 'ok'
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', body.length);
   res.end(body);
 })
-app.listen(3000);
+app.listen(port);
